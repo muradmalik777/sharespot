6843a17ca0e022d7f29dac2a1c1cdf83473d8ca6
Murad Malik <muradmalik7@gmail.com>
2019-06-10 23:52:04 +0500

    space about completed

--- src/components/AboutSpace.vue
+++ src/components/AboutSpace.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-container class="about-space spacing-extra">
+    <v-container grid-list-lg class="about-space spacing-extra">
         <v-layout row wrap>
-            <v-flex xs12 md9 lg9>
+            <v-flex xs12 md8 lg8>
                 <h2 class="title capitalize">About</h2>
                 <p>The Interchange - Atrium is a brand new, state of the art co-working space in 
                 the heart of Camden's famed Stables Market in North London.
@@ -22,8 +22,18 @@
                     <p class="capitalize">Printing</p>
                 </div>
             </v-flex>
-            <v-flex xs12 md3 lg3>
-
+            <v-flex xs12 md4 lg4>
+                <div class="host-price">
+                    <div class="price">
+                        <p class="tag m-b-s">Price</p>
+                        <h2>£200/day <span class="month">£4,000/month</span></h2>
+                    </div>
+                    <div class="host">
+                        <p class="tag m-b-s">Host</p>
+                        <h4>Murad Malik <span><v-icon class="host-icon">call</v-icon><v-icon class="host-icon">chat_bubble_outline</v-icon><v-icon class="host-icon">mail_outline</v-icon></span></h4>
+                    </div>
+                    <v-btn flat class="full-width booking">Book Now</v-btn>
+                </div>
             </v-flex>
         </v-layout>
     </v-container>
@@ -39,6 +49,8 @@ export default {
 }
 </script>
 <style lang="scss">
+@import '../assets/scss/variables.scss';
+
 .about-space{
     max-width: 1300px;
 
@@ -48,6 +60,51 @@ export default {
     .opening-hours, .perks{
         display: inline-block;
         width: 25%;
+    }
+    .host-price{
+        width: 90%;
+        margin: auto !important;
+        min-height: 250px;
+        margin: 30px;
+        background: $light-green;
+        .tag{
+            color: $darkest-green;
+            font-size: 14px;
+        }
+        .month{
+            font-size: 14px;
+            margin-left: 3rem;
+        }
+
+        div{
+            width: 100%;
+            padding: 20px 30px;
+        }
+        .price{
+            border-bottom: 1px solid $white;
+        }
+        .host{
+            span{
+                margin-left: 4rem;
+                .host-icon{
+                    background: $white;
+                    padding: 7px;
+                    border-radius: 100%;
+                    font-size: 18px;
+                    margin-left: .5rem;
+                    cursor: pointer;
+                    &:hover{
+                        color: $dark2;
+                    }
+                }
+            }
+        }
+        .booking{
+            margin: 1.5rem 0 0;
+            background: $dark-green;
+            height: 55px;
+            font-weight: 600;
+        }
     }
 }
 </style>
